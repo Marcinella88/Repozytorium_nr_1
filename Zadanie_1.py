35e283400147c971843c29720347f20cec4b6fd2
Marcin Cz <marcinczapski1988@gmail.com>
2025-02-21 17:09:40 +0100

    Komit dodanie Zadania_1

---
+++ Zadanie_1.py
@@ -0,0 +1,10 @@
+# Zadanie 1 moduł 3
+
+Lista_zakupow = {
+        "piekarnia": ["chleb", "bułka", "pączek"],
+        "warzywniak":["marchew","seler","rukola"]}
+
+print("Lista zakupów")
+for sklepy in Lista_zakupow:
+    print(f"Idę do {sklepy.capitalize()} kupuję tu następujące rzeczy: {[produkty.capitalize() for produkty in Lista_zakupow[sklepy]]}")
+print(f"W sumie kupuję {sum(map(len, Lista_zakupow.values()))} produktów")
